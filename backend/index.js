ed42cd9e272881925743897ffa16c520676f1764
VINAYREDDY4466 <118588257+VINAYREDDY4466@users.noreply.github.com>
2024-12-27 22:47:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use("/api/articles", articleRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });
